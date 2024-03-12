ed3d01c39bee6da2d51f61b373b1549b289ba46c
Atikarn KRUAYKRIANGKRAI <atikarn.kr@ku.th>
2024-03-12 21:42:58 +0700

    add doctest for circle

--- circle.py
+++ circle.py
@@ -6,9 +6,16 @@ class Circle:
 
     def __init__(self, radius):
         """Initialize a circle with given radius.
-        
+
         :param radius: radius of the circle, may be zero.
         :raises ValueError: if radius is negative.
+
+        >>> c1 = Circle(3)
+        >>> c2 = Circle(-3)
+        Traceback (most recent call last):
+        ...
+        ValueError: radius must be non-negative
+
         """
         if radius < 0:
             raise ValueError("radius must be non-negative")
@@ -30,12 +37,24 @@ class Circle:
         return Circle(r)
 
     def get_area(self) -> float:
-        return math.pi*self.radius*self.radius
-    
+        """
+        >>> c1 = Circle(1)
+        >>> c2 = Circle(2)
+        >>> c3 = c1.add_area(c2)
+        >>> c3.get_area()
+        15.707963267948967
+        """
+        return math.pi * self.radius * self.radius
+
     def get_radius(self) -> float:
+        """
+        >>> c1 = Circle(2)
+        >>> c1.get_radius()
+        2
+        """
         return self.radius
 
     def __str__(self) -> str:
         return f"Circle({self.radius})"
-    
+
     __repr__ = __str__
